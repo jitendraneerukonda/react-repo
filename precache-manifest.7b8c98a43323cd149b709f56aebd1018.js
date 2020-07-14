c0933ec4ba79ef830e18e81780d37438e9bbfe81
jitendraneerukonda <jitendraneerukonda@gmail.com>
2020-07-15 03:35:14 +0530

    Updates

---
+++ precache-manifest.7b8c98a43323cd149b709f56aebd1018.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f773f05ce41f7481a76c1c253a4818",
+    "url": "/react-repo/index.html"
+  },
+  {
+    "revision": "9c54cd2d6be7c17b375f",
+    "url": "/react-repo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "080d76e302812a55e94c",
+    "url": "/react-repo/static/js/2.c0aec8e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-repo/static/js/2.c0aec8e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c54cd2d6be7c17b375f",
+    "url": "/react-repo/static/js/main.412b1192.chunk.js"
+  },
+  {
+    "revision": "9764f7a5d46e65ce4650",
+    "url": "/react-repo/static/js/runtime-main.cf3ba26a.js"
+  },
+  {
+    "revision": "714b4c883a957ad3d893aa7bde739310",
+    "url": "/react-repo/static/media/emojiuser.714b4c88.svg"
+  }
+]);
