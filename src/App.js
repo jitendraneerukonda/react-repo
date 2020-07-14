4e3e8117c95a1d507fd4258e45b24a6c824193a8
jitendraneerukonda <jitendraneerukonda@gmail.com>
2020-07-15 04:11:23 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import {BrowserRouter,Route, Link} from 'react-router-dom';
 function App() {
   return(
     <div className = "App">
+      <Home />
       <BrowserRouter>
         <Route exact path ="/" component={Home}/>
         <Route path ="/resume" component={Resume}/>
